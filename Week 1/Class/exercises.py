4a99b908251aad08c06172e864bb2bd214c6de15
elvisyong <elvisyong@gmail.com>
2021-08-19 21:19:43 +0800

    Finish Lab 1

--- Week 1/Class/exercises.py
+++
@@ -1,5 +0,0 @@
-number = int(input("your number: " ))
-
-while number:
-  digit = number % 10000
-  print(digit)
--- Week 1/Class/qns2.py
+++
@@ -1,3 +0,0 @@
-print("Whats your weight in (in kg)? ", 60)
-print("Whats your height in (in m)", 1.70)
-print(f"Your BMI is {60/ (1.70 ** 2)}")
