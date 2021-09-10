19a4bb8a705d91d7dd7827937abea67df10114b4
elvisyong <elvisyong@gmail.com>
2021-09-10 23:54:30 +0800

    Add week 4 Lab qns not finished

---
+++ Week 4/Lab/lab4_starting_code/q1.py
@@ -0,0 +1,29 @@
+month = float(input("Enter the month: "))
+while month > 12 and month < 1:
+  print("Enter a number between 1 and 12 only!")
+  month = float(input("Enter the month: "))
+
+if month == 1:
+  print("There are 31 days in this month")
+elif month == 2:
+  print("There are 28 days in this month")
+elif month == 3:
+  print("There are 31 days in this month")
+elif month == 4:
+  print("There are 30 days in this month")
+elif month == 5:
+  print("There are 31 days in this month")
+elif month == 6:
+  print("There are 30 days in this month")
+elif month == 7:
+  print("There are 31 days in this month")
+elif month == 8:
+  print("There are 31 days in this month")
+elif month == 9:
+  print("There are 30 days in this month")
+elif month == 10:
+  print("There are 31 days in this month")
+elif month == 11:
+  print("There are 30 days in this month")
+elif month == 12:
+  print("There are 31 days in this month")
