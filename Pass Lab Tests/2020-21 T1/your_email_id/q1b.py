be83382684b9b4c5157824d8054fd0af170a2916
elvisyong <elvisyong@gmail.com>
2021-09-21 21:47:27 +0800

    change some stuff

--- Pass Lab Tests/2020-21 T1/your_email_id/q1b.py
+++ Pass Lab Tests/2020-21 T1/your_email_id/q1b.py
@@ -3,20 +3,6 @@
 
 def is_compatible(patient_group, donor_group):
     # Replace the code below with your implementation.
-    if patient_group == "A":
-        if donor_group == "B" or donor_group == "AB":
-            return True
-        return False
-    if patient_group == "B":
-        if donor_group == "B" or donor_group == "AB":
-            return True
-        return False
-    if patient_group == "AB":
-        if donor_group == "AB":
-            return True
-        return False
-    if patient_group == "O":
-        if donor_group == "O" or donor_group == "A" or donor_group == "B" or donor_group == "AB":
-            return True
-        return False
+    if patient_group == "0" or patient_group in donor_group:
+        return True
     return None
