5b45cdf6d42d6ff8b3936090d6340810002dbce8
ElvisYong <elvisyong@gmail.com>
2021-10-22 13:31:05 +0800

    Finish 2b

--- Week 10/Lab/lab7_starting_code/q2b.py
+++ Week 10/Lab/lab7_starting_code/q2b.py
@@ -1,5 +1,12 @@
 ### Q2b: Universities 
 # Write your code below:
-
-
-
+def get_average_num_students(filename):
+    with open(filename, 'r') as f:
+        total = 0
+        count = 0
+        for lines in f:
+            details = lines.split('\t')
+            total += int(details[3])
+            count+=1
+        
+        return total/count
