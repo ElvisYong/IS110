8c48a6247b416d121b2abd21e929728b9064d532
elvisyong <elvisyong@gmail.com>
2021-11-14 16:12:00 +0800

    Finish q2

--- Past Lab Test 2/AY19-20/your_email_id/q2.py
+++ Past Lab Test 2/AY19-20/your_email_id/q2.py
@@ -3,4 +3,10 @@
 
 def get_longer_words(file_name):
     # Modify the code below.
-    return None
+    res = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            words = line.strip('\n').split('&')
+            res.append(words[0] if len(words[0]) > len(words[1])
+                       or len(words[0]) == len(words[1]) else words[1])
+    return res
