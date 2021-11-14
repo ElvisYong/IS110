f9d4b0059aae0fef8fcb9455458f3e998cb9c015
elvisyong <elvisyong@gmail.com>
2021-11-14 16:06:07 +0800

    Finish q1 AY2019

--- Past Lab Test 2/AY19-20/your_email_id/q1.py
+++ Past Lab Test 2/AY19-20/your_email_id/q1.py
@@ -3,5 +3,11 @@
 
 def compute_product(num_list):
     # Modify the code below.
-    return None
+    res = 1
+    if num_list:
+        for num in num_list:
+            if num % 2 != 0:
+                res *= num
+
+    return res
     
