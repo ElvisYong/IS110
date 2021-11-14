ae809630dc952c1e1eb32804c2c6f60ac358dba2
elvisyong <elvisyong@gmail.com>
2021-11-14 16:16:50 +0800

    Finish q3.py

--- Past Lab Test 2/AY19-20/your_email_id/q3.py
+++ Past Lab Test 2/AY19-20/your_email_id/q3.py
@@ -3,4 +3,11 @@
 
 def compute_total_price(price_dict, item_list):
     # Modify the code below.
-    return None
+    total = 0.0
+    for item in item_list:
+        name, quantity = item
+        if name in price_dict:
+            price = price_dict[name]
+            total += price * quantity
+
+    return total
